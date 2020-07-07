19767c35281660825445e57ddecd02dbb34b10b7
anyOne <just@mailbox.org>
2020-07-07 21:16:59 +0200

    first

--- src/app/tabs/meins/meins-page.component.ts
+++ src/app/tabs/meins/meins-page.component.ts
@@ -28,49 +28,64 @@ export class MeinsPage {
     }
 
     onClick($event) {
-        this.load();
-    }
 
-    setColorScheme(scheme) {
-        this.colorScheme = scheme;
-        // Save prefers-color-scheme to localStorage
-        localStorage.setItem('prefers-color', scheme);
-    }
-
-    load() {
-        this.update('light')
-        this.getColorScheme();
-        this.renderer.addClass(document.body, this.colorSchemePrefix + this.colorScheme);
-    }
-
-    getColorScheme() {
-        // Check if any prefers-color-scheme is stored in localStorage
-        if (localStorage.getItem('prefers-color')) {
-            // Save prefers-color-scheme from localStorage
-            this.colorScheme = localStorage.getItem('prefers-color');
+        const systemDark = window.matchMedia('(prefers-color-scheme: dark)');
+        systemDark.addListener(this.colorTest);
+        if (event.detail.checked) {
+            document.body.setAttribute('data-theme', 'dark');
         } else {
-            // If no prefers-color-scheme is stored in localStorage, try to detect OS default prefers-color-scheme
-            this.detectPrefersColorScheme();
+            document.body.setAttribute('data-theme', 'light');
+        }
+
+    }
+
+    colorTest(systemInitiatedDark) {
+        if (systemInitiatedDark.matches) {
+            document.body.setAttribute('data-theme', 'dark');
+        } else {
+            document.body.setAttribute('data-theme', 'light');
         }
     }
 
-    detectPrefersColorScheme() {
-        // Detect if prefers-color-scheme is supported
-        if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
-            // Set colorScheme to Dark if prefers-color-scheme is dark. Otherwise set to light.
-            this.colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-        } else {
-            // If the browser doesn't support prefers-color-scheme, set it as default to dark
-            this.colorScheme = 'dark';
-        }
-    }
-
-    update(scheme) {
-        this.setColorScheme(scheme);
-        // Remove the old color-scheme class
-        this.renderer.removeClass(document.body, this.colorSchemePrefix + (this.colorScheme === 'dark' ? 'light' : 'dark'));
-        // Add the new / current color-scheme class
-        this.renderer.addClass(document.body, this.colorSchemePrefix + scheme);
-    }
+    // setColorScheme(scheme) {
+    //     this.colorScheme = scheme;
+    //     // Save prefers-color-scheme to localStorage
+    //     localStorage.setItem('prefers-color', scheme);
+    // }
+    //
+    // load() {
+    //     this.getColorScheme();
+    //     this.renderer.addClass(document.body, this.colorSchemePrefix + this.colorScheme);
+    // }
+    //
+    // getColorScheme() {
+    //     // Check if any prefers-color-scheme is stored in localStorage
+    //     if (localStorage.getItem('prefers-color')) {
+    //         // Save prefers-color-scheme from localStorage
+    //         this.colorScheme = localStorage.getItem('prefers-color');
+    //     } else {
+    //         // If no prefers-color-scheme is stored in localStorage, try to detect OS default prefers-color-scheme
+    //         this.detectPrefersColorScheme();
+    //     }
+    // }
+    //
+    // detectPrefersColorScheme() {
+    //     // Detect if prefers-color-scheme is supported
+    //     if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
+    //         // Set colorScheme to Dark if prefers-color-scheme is dark. Otherwise set to light.
+    //         this.colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    //     } else {
+    //         // If the browser doesn't support prefers-color-scheme, set it as default to dark
+    //         this.colorScheme = 'dark';
+    //     }
+    // }
+    //
+    // update(scheme) {
+    //     this.setColorScheme(scheme);
+    //     // Remove the old color-scheme class
+    //     this.renderer.removeClass(document.body, this.colorSchemePrefix + (this.colorScheme === 'dark' ? 'light' : 'dark'));
+    //     // Add the new / current color-scheme class
+    //     this.renderer.addClass(document.body, this.colorSchemePrefix + scheme);
+    // }
 
 }
